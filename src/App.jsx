5c2d232246c1394e1cb11365b11d3f5d22383f54
wan <conut22@naver.com>
2025-04-01 23:16:58 +0900

    ♻️ refactor: rename Alert component and related variables to Modal
    - Renamed Alert component to Modal for consistency
    - Updated all related variable and prop names from alert to modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 /** @jsxImportSource @emotion/react */
 import { useState } from 'react';
 
-import Alert from '@components/common/Alert';
 import StackLayout from '@components/common/layout';
+import Modal from '@components/common/Modal';
 import Header from '@components/Header';
-import Media from '@components/Media';
 import NewsRolling from '@components/NewsRolling';
 import DarkModeToggle from '@components/Theme/DarkModeToggle';
 import ThemeProvider from '@components/Theme/ThemeProvider';
@@ -31,7 +30,7 @@ function App() {
         <button onClick={() => setIsModalOpen(true)}>버튼</button>
       </StackLayout>
       {isModalOpen && (
-        <Alert
+        <Modal
           mediaName={targetItem}
           isModalOpen={isModalOpen}
           handleClose={handleClose}
