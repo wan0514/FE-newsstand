83a4a57975d54c3b7c6424b9bc272f12256a2e41
wan <conut22@naver.com>
2025-04-02 23:46:54 +0900

    ✨ feat : render Media Section by prop data
    fetch mockdata yet

---
+++ src/components/Media/MediaSection/ListView/PressInfo.jsx
@@ -0,0 +1,30 @@
+/** @jsxImportSource @emotion/react */
+import styled from '@emotion/styled';
+
+import Button from '@/components/common/Button';
+
+const StyledPressInfo = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 16px;
+`;
+
+const Logo = styled.img`
+  height: 20px;
+`;
+
+const Time = styled.div`
+  ${({ theme }) => theme.typography.m12}
+  color: ${({ theme }) => theme.colors.text.default};
+`;
+
+export default function PressInfo({ logoUrl, editedTime }) {
+  return (
+    <StyledPressInfo>
+      <Logo src={logoUrl} />
+      <Time>{editedTime} 편집</Time>
+      <Button isEmphasized label="구독하기" iconType="plus" />
+    </StyledPressInfo>
+  );
+}
