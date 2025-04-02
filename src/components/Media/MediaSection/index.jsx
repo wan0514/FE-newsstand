130411ea82471e5a8b259e455d5d34066501ee61
wan <conut22@naver.com>
2025-04-02 19:59:24 +0900

    ✨ feat: create grid view UI for dynamic media list rendering
    - Implemented grid-based layout to display media items
    - mediaList prop is used to render media data dynamically

--- src/components/Media/MediaSection/index.jsx
+++ src/components/Media/MediaSection/index.jsx
@@ -1,7 +1,15 @@
+import sampleLogo from '@/assets/logos/sampleLogo.png';
+
 import GridView from './GridView';
 import ListView from './ListView';
 
 const MediaSection = ({ viewType }) => {
+  //test용 언론사 배열
+  const mediaList = Array.from({ length: 24 }, () => ({
+    imageUrl: sampleLogo,
+    name: '언론사이름',
+  }));
+
   function getViewComponent(type) {
     const map = {
       list: ListView,
@@ -12,7 +20,7 @@ const MediaSection = ({ viewType }) => {
 
   const ViewComponet = getViewComponent(viewType);
 
-  return <ViewComponet />;
+  return <ViewComponet mediaList={mediaList} />;
 };
 
 export default MediaSection;
