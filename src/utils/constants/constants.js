f7e91b06bbc5857eb759ebe0ff1bfbfc98c3d28e
wan <conut22@naver.com>
2025-04-07 20:15:42 +0900

    ✨feat: Add category navigation for first/last page in list carousel
    - Added functionality to navigate to the last page of the previous category when at the first page.
    - Added functionality to navigate to the first page of the next category when at the last page.
    - Separated `fieldMap` into a constant file to support category navigation.
    - Extended the `reset` function to accept a page number parameter, enabling direct navigation to a specific page.

---
+++ src/utils/constants/constants.js
@@ -0,0 +1,9 @@
+export const fieldMap = [
+  { id: 1, label: '종합/경제', key: 'generalEconomy' },
+  { id: 2, label: '방송/통신', key: 'broadcastMedia' },
+  { id: 3, label: 'IT', key: 'it' },
+  { id: 4, label: '영자지', key: 'english' },
+  { id: 5, label: '스포츠/연예', key: 'sportsEntertainment' },
+  { id: 6, label: '매거진/전문지', key: 'magazine' },
+  { id: 7, label: '지역', key: 'local' },
+];
