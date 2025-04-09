5850e56e0abe75b9097b1c9bbcfaea6113a3a99f
wan <conut22@naver.com>
2025-04-09 19:01:53 +0900

    ♻️ refactor: Remove redundant code due to changed data from backend
    - Backend data format has changed, leading to unnecessary code.
    - Removed the obsolete code to improve clarity and maintainability.

--- src/components/Media/MediaSection/GridView/index.jsx
+++ src/components/Media/MediaSection/GridView/index.jsx
@@ -1,11 +1,8 @@
-import { useEffect, useState } from 'react';
-
 import GridView from './GridView';
 import Carousel from '../Carousel';
 import useCarousel from '../Carousel/useCarousel';
 
 const GridViewContainer = ({ data: pressList }) => {
-  const [totalPage, setTotalPage] = useState(0);
   const { currentPage, goNext, goPrev } = useCarousel();
 
   const ITEMS_PER_PAGE = 24;
@@ -16,18 +13,12 @@ const GridViewContainer = ({ data: pressList }) => {
 
   const selectedPressData = pressList.slice(startIndex, endIndex);
 
-  useEffect(() => {
-    const totalPageCount = Math.ceil(pressList.length / ITEMS_PER_PAGE);
-    const limitedPageCount = Math.min(totalPageCount, MAX_PAGE);
-    setTotalPage(limitedPageCount - 1);
-  }, [pressList]);
-
   return (
     <Carousel
       goNext={goNext}
       goPrev={goPrev}
       showPrev={currentPage > 0}
-      showNext={currentPage < totalPage}
+      showNext={currentPage < MAX_PAGE - 1}
     >
       <GridView data={selectedPressData} />
     </Carousel>
